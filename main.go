6d8e97679ce6347d1da0c83e15722465865072cf
Thabo <thabo.s.moyo@gmail.com>
2026-08-22 22:44:58 +0100

    separate into functions

--- main.go
+++ main.go
@@ -1,18 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"slices"
+)
+
+// 3x3 board
+// type board struct {
+// 	line [15]string
+// }
 
 func main() {
 	// basic 3x3 board
-
 	for range 2 {
 		fmt.Println(" ")
 	}
 
+	positions := []int{2, 8, 13}
+
+	xo := []string{"X", "O"}
+
+	hLine(positions, xo)
+	xLine(positions, xo)
+	hLine(positions, xo)
+	xLine(positions, xo)
+	hLine(positions, xo)
+}
+
+func hLine(positions []int, xo []string) {
 	for i := range 15 {
-		if i == 5 || i == 10 {
+		reserved := slices.Contains(positions, i)
+
+		if reserved {
+			fmt.Printf("%v", xo[rand.Intn(2)])
+		}
+		if i == 5 {
 			fmt.Print("|")
-		} else {
+		}
+
+		if i == 10 {
+			fmt.Print(" |")
+		}
+
+		if i == 5 || i == 10 && !reserved {
+			// fmt.Print("|")
+		} else if !reserved {
 			fmt.Print(" ")
 		}
 
@@ -20,47 +53,20 @@ func main() {
 			fmt.Println("")
 		}
 	}
+}
 
+func xLine(positions []int, xo []string) {
 	for i := range 15 {
-		if i == 5 || i == 10 {
+		if i == 5 {
 			fmt.Print("|")
-		} else {
+		}
+
+		if i == 10 {
+			fmt.Print("|")
+		}
+
+		if i != 5 || i != 10 {
 			fmt.Print("-")
-		}
-
-		if i == 14 {
-			fmt.Println("")
-		}
-	}
-	for i := range 15 {
-		if i == 5 || i == 10 {
-			fmt.Print("|")
-		} else {
-			fmt.Print(" ")
-		}
-
-		if i == 14 {
-			fmt.Println("")
-		}
-	}
-
-	for i := range 15 {
-		if i == 5 || i == 10 {
-			fmt.Print("|")
-		} else {
-			fmt.Print("-")
-		}
-
-		if i == 14 {
-			fmt.Println("")
-		}
-	}
-
-	for i := range 15 {
-		if i == 5 || i == 10 {
-			fmt.Print("|")
-		} else {
-			fmt.Print(" ")
 		}
 
 		if i == 14 {
